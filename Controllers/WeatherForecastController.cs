f7c9ed3beb7ba44459221c718449e7055697afb1
Mel <eaaalov@students.eaaa.dk>
2024-10-25 10:38:17 +0200

    bummelum

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,29 +4,16 @@ namespace ShippingService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class ShippingServiceController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<ShippingServiceController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public ShippingServiceController(ILogger<ShippingServiceController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
+   
 }
